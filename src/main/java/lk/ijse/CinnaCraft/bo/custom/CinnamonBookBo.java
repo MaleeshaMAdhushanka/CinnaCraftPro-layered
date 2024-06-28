bd41bd005b9cec8576b5eb27e9ba6437c169ab07
Maleesha <zeejmaleeshamadushanka@gmail.com>
2024-06-28 05:53:54 +0530

    Create CinnamonBookBO,Impl

--- src/main/java/lk/ijse/CinnaCraft/bo/custom/CinnamonBookBo.java
+++ src/main/java/lk/ijse/CinnaCraft/bo/custom/CinnamonBookBo.java
@@ -1,2 +1,29 @@
-package lk.ijse.CinnaCraft.bo.custom;public interface CinnamonBookBo {
+package lk.ijse.CinnaCraft.bo.custom;
+
+import lk.ijse.CinnaCraft.Db.DbConnection;
+import lk.ijse.CinnaCraft.Dto.CinnamonBookDto;
+import lk.ijse.CinnaCraft.bo.SuperBo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface CinnamonBookBo extends SuperBo {
+
+     boolean updateCinnamonBookAmount(String CinnamonBookId, double amount) throws SQLException ;
+
+     String getCinnamonBookId(String date) throws SQLException ;
+
+     String getCinnamonBookDate(String CinnamonBookId) throws  SQLException;
+
+     boolean searchDate(String date ) throws SQLException ;
+      boolean createCinnamonBookRecord(String date) throws  SQLException ;
+
+     String generateNextCinnamonBookId() throws  SQLException;
+
+     double getAmount(String string) throws SQLException ;
+     List<CinnamonBookDto> getAllCinnamonBookDetails() throws SQLException ;
 }
--- src/main/java/lk/ijse/CinnaCraft/bo/custom/Impl/CinnamonBookBOImpl.java
+++ src/main/java/lk/ijse/CinnaCraft/bo/custom/Impl/CinnamonBookBOImpl.java
@@ -1,2 +1,65 @@
-package lk.ijse.CinnaCraft.bo.custom.Impl;public class CinnamonBookBOImpl {
+package lk.ijse.CinnaCraft.bo.custom.Impl;
+
+import lk.ijse.CinnaCraft.Dto.CinnamonBookDto;
+import lk.ijse.CinnaCraft.bo.custom.CinnamonBookBo;
+import lk.ijse.CinnaCraft.dao.custom.CinnamonBookDAO;
+import lk.ijse.CinnaCraft.dao.custom.impl.CinnamonBookDAOImpl;
+import lk.ijse.CinnaCraft.entity.CinnamonBook;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CinnamonBookBOImpl implements CinnamonBookBo {
+    CinnamonBookDAO cinnamonBookDAO = new CinnamonBookDAOImpl();
+
+
+    @Override
+    public boolean updateCinnamonBookAmount(String CinnamonBookId, double amount) throws SQLException {
+        return cinnamonBookDAO.updateCinnamonBookAmount(CinnamonBookId, amount);
+    }
+
+    @Override
+    public String getCinnamonBookId(String date) throws SQLException {
+        return cinnamonBookDAO.getCinnamonBookId(date);
+    }
+
+    @Override
+    public String getCinnamonBookDate(String CinnamonBookId) throws SQLException {
+        return cinnamonBookDAO.getCinnamonBookDate(CinnamonBookId);
+    }
+
+    @Override
+    public boolean searchDate(String date) throws SQLException {
+        return cinnamonBookDAO.searchDate(date);
+    }
+
+    @Override
+    public boolean createCinnamonBookRecord(String date) throws SQLException {
+        return cinnamonBookDAO.createCinnamonBookRecord(date);
+    }
+
+    @Override
+    public String generateNextCinnamonBookId() throws SQLException {
+        return cinnamonBookDAO.generateId();
+    }
+
+    @Override
+    public double getAmount(String string) throws SQLException {
+        return cinnamonBookDAO.getAmount(string);
+    }
+
+    @Override
+    public List<CinnamonBookDto> getAllCinnamonBookDetails() throws SQLException {
+        List<CinnamonBook> allCinnamonBookDetails = cinnamonBookDAO.getAll();
+        List<CinnamonBookDto> allCinnamonBookDto = new ArrayList<>();
+        for (CinnamonBook cinnamonBook: allCinnamonBookDetails) {
+            allCinnamonBookDto.add(new CinnamonBookDto(
+                    cinnamonBook.getCinnamonBookId(),
+                    cinnamonBook.getDailyAmount(),
+                    cinnamonBook.getDate()
+            ));
+        }
+        return allCinnamonBookDto;
+    }
 }
